9feb897a53b06c12d1526d0ffd3a0ce53c881588
Gabriel Salas <ggsalas@gmail.com>
2020-11-28 21:24:03 -0300

    migrate domain to ts

---
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import { Global } from '../../contexts/global';
+
+export function PrivateRoute({ component: Component, ...rest }) {
+  const { domain } = useContext(Global);
+  const [user, setUser] = useState(null);
+
+  // needs login on each refresh
+  useEffect(() => {
+    domain
+      .get('currentUserUseCase')
+      .execute()
+      .then(([error, data]) => {
+        if (error) {
+          return window.alert(error.message);
+        }
+        setUser(data);
+      });
+  }, [domain]);
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        user ? (
+          <Component user={user} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+PrivateRoute.propTypes = {
+  component: PropTypes.elementType,
+};
